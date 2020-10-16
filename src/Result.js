b1f0aa93c07c4480ef1fcd6324fbe67ce9ca49d1
Vlp90 <vladimir.leloup@gmail.com>
2020-10-16 12:13:52 +0200

    try to sync data with mapping

--- src/Result.js
+++ src/Result.js
@@ -8,63 +8,49 @@ import { Link } from "react-router-dom";
 import useSearch from "./useSearch";
 import { useStateValue } from "./StateProvider";
 
-
-
-
- function Result()  {
-
+function Result() {
   const [{ term }, dispatch] = useStateValue();
   const { data } = useSearch(term);
 
-  console.log(data);
+  console.log("DATA", data);
 
+  return (
+    <>
+      <Header />
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     result: [],
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   axios
-  //     .get(
-  //       `https://www.superheroapi.com/api.php/10157575892622108/search/ind`
-  //     )
-  //     .then((dbRes) => {
-  //       this.setState({ result: dbRes.data.results });
-  //       console.log(dbRes.data.results);
-  //     })
-  //     .catch((dbErr) => {
-  //       console.log(dbErr);
-  //     });
-  // }
-
-
-    return (
-      <>
-        <Header />
-
-        <div className="result">
-
+      <div className="result">
         <h1>Result(s)</h1>
-          <div className="result__card">
-            {/* {this.state.result.map((element, index) => (
-              <Link to='/:id'>
-              <Card image={element.image.url} name={element.name} />
-              </Link>
-            ))} */}
-          </div>
-          {/* {this.state.result.map((element, index) => (
+        <div className="result__card">
+
+{/* 
+ {data.results.map((item) => ( 
+   <h1>{item.name}</h1>
+   )}; */}
+  
+   {/* {term && (
+   {data.response}
+   )} */}
+{term && (
+   <h1>{data?.response}</h1>
+   {data.results.map((el) => (
+     <h1>{el.name}</h1>
+   ))}
+
+)}
+
+
+
+        </div>
+
+        {/* {this.state.result.map((element, index) => (
           <div className="result__info" key={index}>
             <h1>{element.name}</h1>
             <img src={element.image.url} alt="" />
           </div>
         ))} */}
-        </div>
-      </>
-    );
-  
+      </div>
+    </>
+  );
 }
 
-export default Result
+export default Result;
