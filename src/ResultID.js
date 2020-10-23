ecb68e829469cbe7cb088d8361f220f20a8b7f06
Vlp90 <vladimir.leloup@gmail.com>
2020-10-23 11:55:51 +0200

    structure result id css ok

--- src/ResultID.js
+++ src/ResultID.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./ResultID.css";
 import { useStateValue } from "./StateProvider";
+import FaceIcon from "@material-ui/icons/Face";
 
 import useID from "./useID";
+import { Container, Grid } from "@material-ui/core";
 
 function ResultID() {
   // console.log('REDUCER', reducer.action.id)
@@ -17,15 +19,17 @@ function ResultID() {
     <div className="resultID">
       <div className="resultID__header">
         <div className="resultID__headerBackground">
-            <h3>{dataID?.biography.alignment}</h3>
-            <h3>#{dataID?.id}</h3>
+          <h3>{dataID?.biography.alignment}</h3>
+          <h3>#{dataID?.id}</h3>
         </div>
-          <div className="resultID__headerProfile">
-            <img src={dataID?.image.url} alt="" />
-          </div>
-           <h1>{dataID?.name}</h1> 
+        <div className="resultID__headerProfile">
+          <img src={dataID?.image.url} alt="" />
+        </div>
+        <div className="resultID__headerName">
+          <h1>{dataID?.name}</h1>
+          <h3>({dataID?.biography["full-name"]})</h3>
+        </div>
       </div>
-
 
       {/* {Object.keys(dataID?.appearance).map((keyName, i) => (
     <li className="travelcompany-input" key={i}>
@@ -33,29 +37,41 @@ function ResultID() {
     </li>
 ))} */}
 
-<div className="resultID__info">
-  <div className="resultID__infoLeft">
-  <h2>Appearance</h2>
-      <h3>{dataID?.appearance["eye-color"]}</h3>
-      <h3>{dataID?.appearance.gender}</h3>
-      <h3>{dataID?.appearance["hair-color"]}</h3>
-      <h3>{dataID?.appearance.race}</h3>
-      <h3>{dataID?.appearance.height[1]}</h3>
-      <h3>{dataID?.appearance.weight[1]}</h3>
+      <div className="resultID__appearance">
+        <h2>Appearance</h2>
+        <div className="resultID__appearanceInfo">
+          <Grid container spacing={3}>
+            <Grid item xs={6} sm={4}>
+              <h3>{dataID?.appearance.race}</h3>{" "}
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <h3>{dataID?.appearance.height[1]}</h3>
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <h3>{dataID?.appearance.weight[1]}</h3>
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <h3>{dataID?.appearance.gender}</h3>
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <h3>{dataID?.appearance["eye-color"]}</h3>
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <h3>{dataID?.appearance["hair-color"]}</h3>
+            </Grid>
+          </Grid>
+        </div>
+        <div className="resultID__powerStats">
+          <h2>Powerstats</h2>
+          <h3>{dataID?.powerstats.combat}</h3>
+          <h3>{dataID?.powerstats.durability}</h3>
+          <h3>{dataID?.powerstats.intelligence}</h3>
+          <h3>{dataID?.powerstats.power}</h3>
+          <h3>{dataID?.powerstats.speed}</h3>
+          <h3>{dataID?.powerstats.strength}</h3>
+        </div>
+      </div>
 
-  </div>
-
-  <div className="resultID__infoRight">
-  <h2>Powerstats</h2>
-      <h3>{dataID?.powerstats.combat}</h3>
-      <h3>{dataID?.powerstats.durability}</h3>
-      <h3>{dataID?.powerstats.intelligence}</h3>
-      <h3>{dataID?.powerstats.power}</h3>
-      <h3>{dataID?.powerstats.speed}</h3>
-      <h3>{dataID?.powerstats.strength}</h3>
-
-    </div>
-</div>
       {/* <h2>Appearance</h2>
       <h3>{dataID?.appearance["eye-color"]}</h3>
       <h3>{dataID?.appearance.gender}</h3>
@@ -63,9 +79,8 @@ function ResultID() {
       <h3>{dataID?.appearance.race}</h3>
       <h3>{dataID?.appearance.height[1]}</h3>
       <h3>{dataID?.appearance.weight[1]}</h3> */}
-
+      {/* 
       <h2>Biography</h2>
-      <h3>{dataID?.biography["full-name"]}</h3>
       <h3>{dataID?.biography["alter-egos"]}</h3>
       <h3>{dataID?.biography["first-appearance"]}</h3>
       <h3>{dataID?.biography["place-of-birth"]}</h3>
@@ -75,10 +90,9 @@ function ResultID() {
       <h3>{dataID?.connections["group-affiliation"]}</h3>
       <h3>{dataID?.connections.relatives}</h3>
 
-
       <h2>Work</h2>
       <h3>{dataID?.work.base}</h3>
-      <h3>{dataID?.work.occupation}</h3>
+      <h3>{dataID?.work.occupation}</h3> */}
     </div>
   );
 }
